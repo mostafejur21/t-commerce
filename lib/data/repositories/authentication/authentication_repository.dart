5bb15cf622f284d9ab6f84956e37a0ac3bdd8bb0
Mostafejur Rahman <mostafejur.smuct.cse@gmail.com>
2024-01-11 00:48:30 +0600

    email and password new user registration  function has been added to authentication repository

--- lib/data/repositories/authentication/authentication_repository.dart
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -9,6 +10,7 @@ class AuthenticationRepository extends GetxController{
   static AuthenticationRepository get instance => Get.find();
 
   final devicesStorage = GetStorage();
+  final _auth = FirebaseAuth.instance;
 
   @override
   void onReady() {
@@ -20,4 +22,20 @@ class AuthenticationRepository extends GetxController{
     devicesStorage.writeIfNull("IsFirstTime", true);
     devicesStorage.read("IsFirstTime") != true ? Get.offAll(()=> const LoginScreen()) : Get.offAll(()=> const OnBoardingScreen());
   }
+
+  /*---------------------------Email & password Sign in---------------------------*/
+
+  ///[Email authentication]- Sign in
+  ///[Email authentication]- Register
+  Future<UserCredential> registerWithEmailAndPassword(String email, String password) async{
+    try{
+      return await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    } catch (e){
+      throw "Something went wrong. please try again";
+    }
+  }
+  ///[Email authentication]- Mail verification
+  ///[Email authentication]- Re authenticate user
+  ///[Email authentication]- Forget password
+  /*---------------------------Social Sign in---------------------------*/
 }
